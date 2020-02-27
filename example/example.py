edc871c18bececeac6a100f523cc910a0f43daa7
Fan Feng <fanfeng@dcmb-jieliu2.med.umich.edu>
2020-02-27 12:00:31 -0500

    fixed bugs in loading

--- example/example.py
+++ example/example.py
@@ -8,12 +8,22 @@ from pyHiC.normalization import normalization
 from pyHiC.visualization import visualize_HiC_triangle
 
 
-x = load_HiC('D:\google_cloud\pancreas\chr7_1kb.txt',
-             format='short', chromosome='chr7', resolution=10000,
-             sparse=False, start_pos=0, end_pos=10000000)
-x = np.log(x + 1)
-np.save('chr7_first_10mb_10kb.npy', x)
 
-visualize_HiC_triangle(x, 'test.png', vmax=15)
+x = load_HiC('/nfs/turbo/umms-drjieliu/proj/4dn/data/bulkHiC/H1-hESC/processed/chr16_1kb.txt',
+             format='short', chromosome='chr7', resolution=5000, sparse=False,
+             start_pos=22000000, end_pos=23500000
+             )
+x = np.log(x + 1)
+# np.save('chr16_25M_40M_5kb.npy', x)
+
+# x = np.load('chr16_25M_40M_5kb.npy')
+print(x.shape)
+visualize_HiC_triangle(x, 'chr16_17M_32M_5kb.png', vmax=4, colorbar=False)
+
+# y = x[900:1200, 900:1200]
+# visualize_HiC_triangle(y, 'chr7_29.5M_31M_5kb.png', vmax=5, colorbar=False)
+
+# z = x[995:1045, 995:1045]
+# visualize_HiC_triangle(z, 'chr7_29.975M_30.225M_5kb.png', vmax=5, colorbar=False)
 
 
