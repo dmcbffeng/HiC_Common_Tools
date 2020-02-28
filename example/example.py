ae12959eedbbe93d0969d69c08308feb01d4ab45
dcmbffeng <fanfeng@umich.edu>
2020-02-27 20:58:10 -0500

    fixed bugs in HiCRep

--- example/example.py
+++ example/example.py
@@ -6,14 +6,19 @@ from pyHiC.loading import load_HiC
 from pyHiC.structures import AB_compartment
 from pyHiC.normalization import normalization
 from pyHiC.visualization import visualize_HiC_triangle
+from pyHiC.reproducibility import HiCRep
 
 
-x = load_HiC('D:\google_cloud\pancreas\chr7_1kb.txt',
-             format='short', chromosome='chr7', resolution=10000,
-             sparse=False, start_pos=0, end_pos=10000000)
-x = np.log(x + 1)
-np.save('chr7_first_10mb_10kb.npy', x)
+# x = load_HiC('D:\google_cloud\pancreas\chr7_1kb.txt',
+#              format='short', chromosome='chr7', resolution=10000,
+#              sparse=False, start_pos=0, end_pos=10000000)
+# x = np.log(x + 1)
+# np.save('chr7_first_10mb_10kb.npy', x)
+#
+# visualize_HiC_triangle(x, 'test.png', vmax=15)
 
-visualize_HiC_triangle(x, 'test.png', vmax=15)
+x, y = np.random.random((100, 100)), np.random.random((100, 100))
+x, y = x + x.T, y + y.T
 
+print(HiCRep(x, y))
 
--- pyHiC/reproducibility/reproducibility.py
+++ pyHiC/reproducibility/reproducibility.py
@@ -27,13 +27,17 @@ def HiCRep(HiC1, HiC2, n_strata=10, h=1):
             for r, c, v in zip(_mat.row, _mat.col, _mat.data):
                 if abs(r - c) < _n_strata and r <= c:
                     _str[abs(r - c)][r] = v
+        else:
+            for i in range(_n_strata):
+                _str[i] = np.diag(_mat[i:, :_l-i])
         return _str
 
     strata1, strata2 = _strata(HiC1, n_strata, sparse1), _strata(HiC2, n_strata, sparse2)
+    print(strata1[0], strata2[0])
     std1, std2 = np.array([np.std(_s1) for _s1 in strata1]), np.array([np.std(_s2) for _s2 in strata2])
     lengths = np.arange(HiC1.shape[0], HiC1.shape[0] - n_strata, -1)
     weights = std1 * std2 * lengths
     Pearson_corrs = np.array([np.corrcoef(_s1, _s2)[0][1] for _s1, _s2 in zip(strata1, strata2)])
-    score = weights * Pearson_corrs / np.sum(weights)
+    score = np.sum(weights * Pearson_corrs / np.sum(weights))
     return score
 
